a04bf3925ba1abfe306b60faa2fcc1504c38fc89
Evgeniy Shevtsov <evheniy.shevtsov@gmail.com>
2022-02-03 00:19:05 +0300

    Add TODO after review

--- src/A_4/PublicBuilding.java
+++ src/A_4/PublicBuilding.java
@@ -1,4 +1,5 @@
 package A_4;
 
+// TODO не придумал что можно использовать из методов/полей? получается абсолютно бесполезная и пустая абстракция
 abstract class PublicBuilding implements Building {
 }
--- src/A_6/AbstractShip.java
+++ src/A_6/AbstractShip.java
@@ -1,4 +1,5 @@
 package A_6;
 
+// TODO те же вопросы что и к PublicBuilding
 abstract class AbstractShip implements Ship {
 }
